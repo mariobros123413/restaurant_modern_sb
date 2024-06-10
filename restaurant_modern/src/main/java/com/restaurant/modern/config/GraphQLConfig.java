1f0f61decb1119edbe5818efeb5f91585c5cd2e3
mariobros123413 <112135898+mariobros123413@users.noreply.github.com>
2024-06-09 21:27:40 -0400

    graphqls separate

--- restaurant_modern/src/main/java/com/restaurant/modern/config/GraphQLConfig.java
+++ restaurant_modern/src/main/java/com/restaurant/modern/config/GraphQLConfig.java
@@ -38,71 +38,67 @@ public class GraphQLConfig {
 	}
 
 	@Bean
-	public GraphQlSource graphQlSource(RuntimeWiring.Builder runtimeWiringBuilder) throws IOException {
-		SchemaParser schemaParser = new SchemaParser();
-		SchemaGenerator schemaGenerator = new SchemaGenerator();
+    public GraphQlSource graphQlSource(RuntimeWiring.Builder runtimeWiringBuilder) throws IOException {
+        SchemaParser schemaParser = new SchemaParser();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
 
-		TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
-		Resource schemaResourceFactura = new ClassPathResource("graphql/schema.graphqls");
+        TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
 
-		typeRegistry.merge(schemaParser.parse(schemaResourceFactura.getInputStream()));
+        // Cargar y combinar los esquemas de los archivos .graphqls
+        Resource[] schemaResources = new Resource[] {
+            new ClassPathResource("graphql/schemaUsuario.graphqls"),
+            new ClassPathResource("graphql/schemaMesa.graphqls"),
+            new ClassPathResource("graphql/schemaFactura.graphqls")
+        };
 
-		// Configurar el RuntimeWiring
-		RuntimeWiring runtimeWiring = runtimeWiringBuilder
-				.type("Query",
-						typeWiring -> typeWiring
-								.dataFetcher("facturas", environment -> facturaResolver.getFacturas())
-								.dataFetcher("factura",	environment -> facturaResolver.getFactura(environment.getArgument("nro")))
-								.dataFetcher("usuarios", environment -> usuarioResolver.getUsuarios())
-								.dataFetcher("usuario",	environment -> usuarioResolver.getUsuario(environment.getArgument("id")))
-								.dataFetcher("mesas", environment -> mesaResolver.getMesas())
-								.dataFetcher("mesaByNro", environment -> mesaResolver.getMesaByNro(environment.getArgument("nro")))
-								.dataFetcher("mesa",	environment -> mesaResolver.getMesa(environment.getArgument("id"))))
-				.type("Mutation",
-						typeWiring -> typeWiring
-								.dataFetcher("createFactura",
-										environment -> facturaResolver.createFactura(
-												environment.getArgument("id_usuario"), environment.getArgument("total"),
-												environment.getArgument("fecha"), environment.getArgument("pedido")))
-								.dataFetcher("createMesa",
-										environment -> mesaResolver.createMesa(
-												environment.getArgument("id_usuario"), environment.getArgument("nro"),
-												environment.getArgument("capacidad"), environment.getArgument("disponible")))
-								.dataFetcher("updateMesa",
-										environment -> mesaResolver.updateMesa(
-												environment.getArgument("id"), environment.getArgument("nro"),
-												environment.getArgument("capacidad"), environment.getArgument("disponible")))
-								.dataFetcher("updateMesaByNro",
-										environment -> mesaResolver.updateMesaByNro(
-												environment.getArgument("nro"),
-												environment.getArgument("capacidad"), environment.getArgument("disponible")))
-								.dataFetcher("deleteMesa",
-										environment -> mesaResolver.deleteMesa(
-												environment.getArgument("id")))
-								.dataFetcher("updateFactura",
-										environment -> facturaResolver.updateFactura(
-												environment.getArgument("nro"),
-												environment.getArgument("total"), environment.getArgument("fecha")))
-								.dataFetcher("deleteFactura",
-										environment -> facturaResolver.deleteFactura(
-												environment.getArgument("nro")))
-								.dataFetcher("createUsuario",
-										environment -> usuarioResolver.createUsuario(
-												environment.getArgument("nombreUsuario"),
-												environment.getArgument("password"), environment.getArgument("admin")))
-								.dataFetcher("updateUsuario",
-										environment -> usuarioResolver.updateUsuario(
-												environment.getArgument("id"),
-												environment.getArgument("nombre_usuario"), environment.getArgument("password"),
-												environment.getArgument("isAdmin")))
-								.dataFetcher("deleteUsuario",
-										environment -> usuarioResolver.deleteUsuario(
-												environment.getArgument("id"))))
-				.build();
+        for (Resource schemaResource : schemaResources) {
+            typeRegistry.merge(schemaParser.parse(schemaResource.getInputStream()));
+        }
 
-		GraphQLSchema schema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
-		return GraphQlSource.builder(schema).build();
-	}
+        // Configurar el RuntimeWiring
+        RuntimeWiring runtimeWiring = runtimeWiringBuilder
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("facturas", environment -> facturaResolver.getFacturas())
+                        .dataFetcher("factura", environment -> facturaResolver.getFactura(environment.getArgument("nro")))
+                        .dataFetcher("usuarios", environment -> usuarioResolver.getUsuarios())
+                        .dataFetcher("usuario", environment -> usuarioResolver.getUsuario(environment.getArgument("id")))
+                        .dataFetcher("mesas", environment -> mesaResolver.getMesas())
+                        .dataFetcher("mesaByNro", environment -> mesaResolver.getMesaByNro(environment.getArgument("nro")))
+                        .dataFetcher("mesa", environment -> mesaResolver.getMesa(environment.getArgument("id"))))
+                .type("Mutation", typeWiring -> typeWiring
+                        .dataFetcher("createFactura", environment -> facturaResolver.createFactura(
+                                environment.getArgument("id_usuario"), environment.getArgument("total"),
+                                environment.getArgument("fecha"), environment.getArgument("pedido")))
+                        .dataFetcher("createMesa", environment -> mesaResolver.createMesa(
+                                environment.getArgument("id_usuario"), environment.getArgument("nro"),
+                                environment.getArgument("capacidad"), environment.getArgument("disponible")))
+                        .dataFetcher("updateMesa", environment -> mesaResolver.updateMesa(
+                                environment.getArgument("id"), environment.getArgument("nro"),
+                                environment.getArgument("capacidad"), environment.getArgument("disponible")))
+                        .dataFetcher("updateMesaByNro", environment -> mesaResolver.updateMesaByNro(
+                                environment.getArgument("nro"),
+                                environment.getArgument("capacidad"), environment.getArgument("disponible")))
+                        .dataFetcher("deleteMesa", environment -> mesaResolver.deleteMesa(
+                                environment.getArgument("id")))
+                        .dataFetcher("updateFactura", environment -> facturaResolver.updateFactura(
+                                environment.getArgument("nro"),
+                                environment.getArgument("total"), environment.getArgument("fecha")))
+                        .dataFetcher("deleteFactura", environment -> facturaResolver.deleteFactura(
+                                environment.getArgument("nro")))
+                        .dataFetcher("createUsuario", environment -> usuarioResolver.createUsuario(
+                                environment.getArgument("nombreUsuario"),
+                                environment.getArgument("password"), environment.getArgument("admin")))
+                        .dataFetcher("updateUsuario", environment -> usuarioResolver.updateUsuario(
+                                environment.getArgument("id"),
+                                environment.getArgument("nombre_usuario"), environment.getArgument("password"),
+                                environment.getArgument("isAdmin")))
+                        .dataFetcher("deleteUsuario", environment -> usuarioResolver.deleteUsuario(
+                                environment.getArgument("id"))))
+                .build();
+
+        GraphQLSchema schema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+        return GraphQlSource.builder(schema).build();
+    }
 
 	private String loadSchema(String path) throws IOException {
 		Resource resource = new ClassPathResource(path);
